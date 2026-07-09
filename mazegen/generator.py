544859bc11fdd4c2550f9af963007169dc4804b8
Hasan Romani <hromani@c1r4s8.42beirut.com>
2026-07-09 21:33:37 +0300

    a

--- mazegen/generator.py
+++ mazegen/generator.py
@@ -48,13 +48,61 @@ class MazeGenerator():
         """Checks if the given cell is inbound for the carve function"""
         dx, dy = direction.delta
         nx, ny = dx + x, dy + y
-        return (nx in range(0, self.width)) and (ny in range(0, self.height))
+        return (nx in range(0, self.width)
+                ) and (ny in range(0, self.height))
 
     def carve(self, x: int, y: int, direction: Direction) -> None:
         """Carves the given wall from the given cell and its adjacent"""
         if not self.valid_bound(x, y, direction):
-            return
+            raise ValueError("Out of bounds")
         self.grid[y][x] &= ~(1 << direction)
         dx, dy = direction.delta
         nx, ny = dx + x, dy + y
         self.grid[ny][nx] &= ~(1 << direction.opposite)
+
+    def push(self, cell: tuple[int, int],
+             stack: list[tuple[int, int]], visited: list[list[bool]]) -> None:
+        """Pushes a cell into the stack and marks it visited"""
+        stack.append(cell)
+        visited[cell[1]][cell[0]] = True
+
+    def get_neighbors(self, cell: tuple[int, int], visited:
+                      list[list[bool]]) -> list[tuple[int, int, Direction]]:
+        """"Gets the unvisited neighbors of a cell"""
+        unvisited: list[tuple[int, int, Direction]] = []
+        for direction in Direction:
+            dx, dy = direction.delta
+            nx, ny = cell[0] + dx, cell[1] + dy
+            if self.valid_bound(cell[0], cell[1],
+                                direction) and not visited[ny][nx]:
+                unvisited.append((nx, ny, direction))
+        return unvisited
+
+    def generate(self) -> None:
+        """Generates a maze from the grid"""
+        visited: list[list[bool]] = [[False] * self.width
+                                     for _ in range(self.height)]
+        stack: list[tuple[int, int]] = []
+        self.push(self.entry, stack, visited)
+        while stack:
+            neighbors = self.get_neighbors(stack[-1], visited)
+            if neighbors:
+                chosen_cell = self._rng.choice(neighbors)
+                self.carve(stack[-1][0], stack[-1][1], chosen_cell[2])
+                self.push((chosen_cell[0], chosen_cell[1]), stack, visited)
+            else:
+                stack.pop()
+
+
+if __name__ == "__main__":
+    gen = MazeGenerator(10, 10, (0, 0), (3, 2), True, 42)
+    gen2 = MazeGenerator(10, 10, (0, 0), (3, 2), True, 42)
+    gen.generate()
+    print("1st maze")
+    hex_grid = [[f"{num:X}" for num in sublist] for sublist in gen.grid]
+    print(*hex_grid, sep="\n")
+    print("2nd maze")
+    gen2.generate()
+    hex_grid2 = [[f"{num:X}" for num in sublist] for sublist in gen2.grid]
+    print(*hex_grid2, sep="\n")
+
